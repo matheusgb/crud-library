6cbd62c5d55e74cee5b04611cf5cd82fbcb952e1
matheusgb <matheusgbr98@gmail.com>
2022-09-01 01:19:12 -0300

    feat :sparkles:: handler update created

---
+++ handlers/update.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/matheusgb/crud-library/models"
+)
+
+func Update(writer http.ResponseWriter, request *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(request, "id"))
+	if err != nil {
+		log.Printf("Error parse id: %v", err)
+		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	var book models.Book
+
+	err = json.NewDecoder(request.Body).Decode(&book)
+	if err != nil {
+		log.Printf("Error while decoding json: %v", err)
+		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	rows, err := models.Update(id, book)
+	if err != nil {
+		log.Printf("Update error: %v", err)
+		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if rows > 1 {
+		log.Printf("Error: updated %d records", rows)
+	}
+
+	response := map[string]any{
+		"Error":   false,
+		"Message": "Successfully updated!",
+	}
+
+	writer.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(response)
+}
